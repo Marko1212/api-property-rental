97f452f5362125b2e11fd863c95652bb2a5e8a5b
Marko1212 <maskovic@thefeedbackinstitute.com>
2022-09-10 13:44:56 +0100

    further development

---
+++ src/Events/PasswordHasherSubscriber.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Events;
+
+use ApiPlatform\Core\EventListener\EventPriorities;
+use App\Entity\User;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class PasswordHasherSubscriber implements EventSubscriberInterface
+{
+    private $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::VIEW => ['hashPassword', EventPriorities::PRE_WRITE]
+        ];
+    }
+
+    public function hashPassword(ViewEvent $event)
+    {
+        $result = $event->getControllerResult();
+        $method = $event->getRequest()->getMethod();
+
+        if ($result instanceof User && ($method === 'POST')) {
+            $hash = $this->hasher->hashPassword($result, $result->getPassword());
+            $result->setPassword($hash);
+        }
+    }
+}
