10bc58ba1089f2d5b32e55b862a119205ac4eb83
Marko1212 <maskovic@thefeedbackinstitute.com>
2022-09-11 14:29:13 +0100

    added Repository functions testing

---
+++ tests/Repository/PropertyRepositoryTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Tests\Repository;
+
+use App\Entity\Property;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class PropertyRepositoryTest extends KernelTestCase
+{
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    private $entityManager;
+
+    protected function setUp(): void
+    {
+        $kernel = self::bootKernel();
+
+        $this->entityManager = $kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+    }
+
+    public function testGetChosenPropertiesCreatedByManagers()
+    {
+        $properties = $this->entityManager
+            ->getRepository(Property::class)
+            ->getChosenPropertiesCreatedByManagers();
+
+        $this->assertSame([], $properties);
+    }
+
+    public function testGetChosenPropertyNamesCreatedByAdmins()
+    {
+        $propertyNames = $this->entityManager
+            ->getRepository(Property::class)
+            ->getChosenPropertyNamesCreatedByAdmins();
+
+        $this->assertSame([['name' => 'Moja draga Apolinarija'], ['name' => 'Moja draga Olga']], $propertyNames);
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        // doing this is recommended to avoid memory leaks
+        $this->entityManager->close();
+        $this->entityManager = null;
+    }
+}
