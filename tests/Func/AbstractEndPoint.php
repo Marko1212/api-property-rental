878437095b860c132aea5b68968b7f5000a91738
Marko1212 <maskovic@thefeedbackinstitute.com>
2022-09-12 09:05:17 +0100

    functional test on POST property

--- tests/Func/AbstractEndPoint.php
+++ tests/Func/AbstractEndPoint.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 abstract class AbstractEndPoint extends WebTestCase
 {
-    private array $serverInformations = ['ACCEPT' => 'application/json', 'CONTENT_TYPE' => 'application/json', 'HTTP_AUTHORIZATION' => 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE2NjI5NjUyMTYsImV4cCI6MTY2Mjk2ODgxNiwicm9sZXMiOlsiUk9MRV9BRE1JTiIsIlJPTEVfVVNFUiJdLCJ1c2VybmFtZSI6IkxlZHVjLlZhbGVyaWVAVmFpbGxhbnQub3JnIn0.mrunV4zyF8I17Tpq8ayFrd5t_IB58k8uE_egg_Di7FpakQVe5nrWf4lAvgIoV0OwPIJtgtVEs81AzMexfxkq3rJns6Aeo8cO6p0f29u6Ci21NB9u_OFa5P_Wiz11e4_gS3LKrI68aKRSew0xnbcNYaHAAKC5LBerd7225zMo1nVScTL_zkzvyov7GKY-jWO_8wKB-ee9PepSpInWU2JgVAwlD6maLMRc7VRinZ8lkfLpxn8ss7HUWuD40pYMM8PsTehRj-AtDDtn7VSzeoRUmZ2CA_UO_MqBgzSZM0Eg5qFK2A7Wi0Z_49ha2S-OhFalb9CBJ5aJTXzkJdD9Gh-zGA'];
+    private array $serverInformations = ['ACCEPT' => 'application/json', 'CONTENT_TYPE' => 'application/json', 'HTTP_AUTHORIZATION' => 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE2NjI5Njk0MzIsImV4cCI6MTY2Mjk3MzAzMiwicm9sZXMiOlsiUk9MRV9BRE1JTiIsIlJPTEVfVVNFUiJdLCJ1c2VybmFtZSI6IkxlZHVjLlZhbGVyaWVAVmFpbGxhbnQub3JnIn0.VFO9JePeoreCSojtwL-fmM3hYqKDlJaxtZx30fvmTWtrhShJv-nJeJGdpjxDkQPAQr9IcO9qX7uK1RlXjQ9P154BBEz26wwT1B8jmHb7mDaifgyZ3Mc00HCCNm8P71HLRiHK3le8NYpTBBSBCknwc34TbOoEYMI_xh5mu6D-_dGrHlQKTggqIfwVu2qfOAFolh7feRYJ3uD_7wJkmntmwylbJjunbSVMft5fQ9OXj-wMaumfJoheNKCJmcOpwTZ84AD0wjLVjWGropTgepom6YhgiCGDSsHCCYATqefdSp_aRwVOmQ-yIB66soYTNgYk1zcfFtcP8cq3UqDp6a5quw'];
 
     public function getResponseFromRequest(string $method, string $uri, string $payload = ''): Response
     {
--- tests/Func/PropertyTest.php
+++ tests/Func/PropertyTest.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 class PropertyTest extends AbstractEndPoint
 {
-    private string $propertyPayLoad = '{"name": "Appartement T3 à louer",
+    private string $propertyPayload = '{"name": "%s",
         "city": "Valenciennes",
         "street": "rue Gérard de Perfontaine",
         "price": 500,
@@ -27,12 +27,18 @@ class PropertyTest extends AbstractEndPoint
 
     public function testPostProperty(): void
     {
-        $response = $this->getResponseFromRequest(Request::METHOD_POST, '/api/properties', $this->propertyPayLoad);
+        $response = $this->getResponseFromRequest(Request::METHOD_POST, '/api/properties', $this->getPayload());
         $responseContent = $response->getContent();
         $responseDecoded = json_decode($responseContent);
-        dd($responseDecoded);
-        self::assertEquals(Response::HTTP_OK, $response->getStatusCode());
+
+        self::assertEquals(Response::HTTP_CREATED, $response->getStatusCode());
         self::assertJson($responseContent);
         self::assertNotEmpty($responseDecoded);
+    }
+
+    private function getPayload()
+    {
+        $faker = Factory::create('fr_FR');
+        return sprintf($this->propertyPayload, $faker->sentence(10));
     }
 }
